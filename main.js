2014c62cb51938af319caffd0e1cd0313d9697b2
Van Hoang <111828642+vhoang11@users.noreply.github.com>
2022-10-09 23:05:36 -0500

    moved to right repository

--- main.js
+++ main.js
@@ -1,8 +1,1 @@
-window.toggleLeft = function() {
-  var element = document.getElementById("offcanvas-left");
-  element.classList.toggle("hide");
-}
-window.toggleRight = function() {
-  var element = document.getElementById("offcanvas-right");
-  element.classList.toggle("hide");
-}
+console.log("Practicing");
